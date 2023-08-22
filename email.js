0f6517f89b9c3a51a6834f29ed9e47ed5912837d
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-08-22 19:35:43 +0530

    Update email.js

--- email.js
+++ email.js
@@ -1,22 +1,25 @@
 const nodemailer = require('nodemailer');
 
+// Step 1: Generate an App Password from Gmail
+// Visit: https://myaccount.google.com/security
+// In the "Signing in to Google" section, enable "2-Step Verification" if not already enabled.
+// Then, go to "App passwords", select "Mail" and your "Device", and generate an app password.
+// Use this generated app password in the 'pass' field below.
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'your-email@gmail.com',
-    pass: 'your-email-password',
+    pass: 'your-generated-app-password', // Use the app password you generated
   },
 });
-
 
 const mailOptions = {
   from: 'your-email@gmail.com',
   to: 'recipient@example.com',
   subject: 'Testing Nodemailer',
-  text: 'This is a test email sent using Nodemailer.',
+  text: 'This is a test email sent using Nodemailer.', // You can replace the text with email content
 };
-
 
 transporter.sendMail(mailOptions, (error, info) => {
   if (error) {
